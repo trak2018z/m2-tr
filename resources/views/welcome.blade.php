1bf325290ecb5211a4744de024e26cbbe868cdc6
HubertSadecki <hubertsad@o2.pl>
2017-11-05 18:20:08 +0100

    Project Init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>M2</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+                text-align: center;
+            }
+
+        </style>
+    </head>
+    <body>
+        <h3>This site is under construction</h3>
+    </body>
+</html>
